4527f7602f9d7ee632d057bd2a7bf6fa052c53de
FrancescoF <98francescofurini@gmail.com>
2024-07-04 12:04:35 +0200

    commit di main.tf e buildspec.yml iniziale

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_instance" "macchina_prova_build" {
+  ami           = "ami-052387465d846f3fc"  # linux free tier
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "macchina_prova_build"
+  }
+} 
